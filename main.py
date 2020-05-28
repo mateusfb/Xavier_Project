28af3f798b3f9a2a25b9bb3e873608af4f29ed57
Mateus Firmino <mateusfirminob@gmail.com>
2020-05-28 18:33:46 +0000

    Mudanças no output

--- main.py
+++ main.py
@@ -36,37 +36,24 @@ def dict_from_json(src):
 
     return dictionary
 
+def plot_program(program):
+  labels, values = zip(*program.items())
+  total = sum(values)
+  plt.title("Distribuição de qualis por programa", bbox={'facecolor':'0.8', 'pad':5})
+  plt.pie(values, startangle=90,labels=labels,
+  autopct=lambda p : "{:.1f}%\n({:d})".format(p,int(round((p*total)/100))))
+  plt.axis('equal')
+  plt.savefig('graph.png')
+
+"""
 dataframe = pd.read_csv("Particao_k3_c1_reduced.csv")
 dictionary = count_qualis_occurances_for_program(dataframe)
 dict_to_json("Particao_k3_c1_reduced.json", dictionary)
-
-def plot_program(program):
-	labels, values = zip(*program.items())
-	total = sum(values)
-	sizes = [qualis_count / total for qualis_count in values]
-	fig1, ax1 = plt.subplots()
-	ax1.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90)
-	ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-	plt.savefig('graph.png')
+"""
 
 plot_program(dict_from_json("Particao_k3_c1_reduced.json")["23001011005P7"])
 
 """
 programs_dict = dict_from_json("dict.json")
 print(programs_dict)
-"""
-
-"""
-pegue uma linha
-para x em programas {
-	verifique se x é uma chave no dicionario(true){
-		verificar se o qualis está no dicionario do programa(true){
-			adicionar um ao valor da chave do qualis
-		} else {
-			adicionar a chave ao dicionario e setar 1
-		}
-	} se não {
-		adiciona o programa com um dicionario vazio
-	}
-}
 """
