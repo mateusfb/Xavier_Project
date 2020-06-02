3020da725143cefb56bfff36976d800b753cad67
mateusfb <mateusfirminob@gmail.com>
2020-06-02 17:10:51 -0300

    Mudança para gráfico em barra

--- summarizer.py
+++ summarizer.py
@@ -5,6 +5,9 @@ import matplotlib.pyplot as plt
 import json
 import sys
 
+def string_to_list(str):
+  return str.strip('][').split(",")
+
 def file_name_from_path(path):
     head, tail = split(path)
     filename = tail or basename(head)
@@ -27,12 +30,17 @@ def count_qualis_occurances_for_programs(src):
     programs_list = split_programs(dataframe["programas"][i])
     for program in programs_list:
       if program in programs_dict:
-        if qualis in programs_dict[program]:
-          programs_dict[program][qualis] += 1
-        else:
-          programs_dict[program][qualis] = 1
+        programs_dict[program][qualis] += 1
       else:
-        programs_dict[program] = {qualis : 1}
+        programs_dict[program] = {"A1": 0, 
+                                  "A2": 0, 
+                                  "A3": 0, 
+                                  "A4": 0, 
+                                  "B1": 0, 
+                                  "B2": 0, 
+                                  "B3": 0, 
+                                  "B4": 0, 
+                                  "C/NI/NP": 0}
   return programs_dict
 
 def dict_to_json(filename, dictionary):
@@ -53,20 +61,57 @@ def make_dict(csv_file, json_filename = None):
 
   dict_to_json(json_filename, count_qualis_occurances_for_programs(csv_file))
 
+def make_qualis_count_list(dictionary):
+  count_dict = {"A1": [], 
+                "A2": [], 
+                "A3": [], 
+                "A4": [], 
+                "B1": [], 
+                "B2": [], 
+                "B3": [], 
+                "B4": [], 
+                "C/NI/NP": []}
+  for key in dictionary:
+    for qualis in count_dict:
+      count_dict[qualis].append(dictionary[key][qualis])
 
-def plot_program(dict_src, program_code, image_name = None):
-  program = dict_from_json(dict_src)[program_code]
-  labels, values = zip(*program.items())
-  total = sum(values)
-  plt.title("Distribuição de qualis por programa", bbox={'facecolor':'1', 'pad':5})
-  wedges, text, autotext = plt.pie(values, startangle=90,
-  autopct=lambda p : "{:.1f}%\n({:d})".format(p,int(round((p*total)/100))))
-  plt.axis('equal')
-  plt.legend(wedges, labels, title="Qualis", loc="center left", bbox_to_anchor=(0.85, 0, 0.5, 1))
+  return count_dict
+
+
+def plot_program(dict_src, program_code_list_str, image_name = None):
+  program_code_list = string_to_list(program_code_list_str)
+  subdict = {key : value for key, value in dict_from_json(dict_src).items() if key in program_code_list}
+  count_dict = make_qualis_count_list(subdict)
+
+  fig, ax = plt.subplots()
+  x = np.arange(len(program_code_list))
+  bar_width = 0.08
+  multiplier = 0
+  colors = ["#0d340d", "#165816", "#32c732", "#48bfe3", "#5e60ce", "#7400b8", "#4d0400", "#a80900", "#ff3d33"]
+
+  for qualis in count_dict:
+    ax.bar(x + bar_width * multiplier, count_dict[qualis], width=bar_width, label=qualis,
+           color=colors[multiplier])
+    multiplier += 1
+
+  ax.set_xticks(x + 4 * bar_width)
+  ax.set_xticklabels(program_code_list)
+  ax.legend()
+
+  ax.spines['top'].set_visible(False)
+  ax.spines['right'].set_visible(False)
+  ax.spines['left'].set_visible(False)
+  ax.spines['bottom'].set_color('#DDDDDD')
+  ax.tick_params(bottom=False, left=False)
+  ax.set_axisbelow(True)
+  ax.yaxis.grid(True, color='#DDDDDD')
+  ax.set_title('Produções por programa')
+
+  fig.tight_layout()
 
   if image_name is None:
-    image_name = file_name_from_path(dict_src) + "_" + program_code + ".png"
-  plt.savefig(image_name)
+    image_name = file_name_from_path(dict_src) + "_" + str(program_code_list) + ".png"
+  fig.savefig(image_name)
 
 if __name__ == '__main__':
   globals()[sys.argv[1]](*sys.argv[2:])
